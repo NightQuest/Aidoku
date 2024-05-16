1be6e6221ffcaf76aae2c4e8163832d07fa2121a
Amelia Frost <afrosty.skye@gmail.com>
2024-05-15 19:18:54 -0700

    Migrate, as well as Mark all as read/unread can now be accessed within the library directly

--- iOS/UI/Library/LibraryViewController.swift
+++ iOS/UI/Library/LibraryViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import LocalAuthentication
+import SwiftUI
 
 class LibraryViewController: MangaCollectionViewController {
 
@@ -976,6 +977,40 @@ extension LibraryViewController {
                     )
                 })
             }
+
+            actions.append(UIMenu(title: NSLocalizedString("MARK_ALL", comment: ""), image: nil, children: [
+                // read chapters
+                UIAction(title: NSLocalizedString("READ", comment: ""), image: UIImage(systemName: "eye")) { _ in
+                    self.showLoadingIndicator()
+                    Task {
+                        let manga = manga.toManga()
+                        let chapters = await CoreDataManager.shared.getChapters(sourceId: manga.sourceId, mangaId: manga.id)
+
+                        await HistoryManager.shared.addHistory(chapters: chapters)
+                        self.hideLoadingIndicator()
+                    }
+                },
+                // unread chapters
+                UIAction(title: NSLocalizedString("UNREAD", comment: ""), image: UIImage(systemName: "eye.slash")) { _ in
+                    self.showLoadingIndicator()
+                    Task {
+                        let manga = manga.toManga()
+                        let chapters = await CoreDataManager.shared.getChapters(sourceId: manga.sourceId, mangaId: manga.id)
+
+                        await HistoryManager.shared.removeHistory(chapters: chapters)
+                        self.hideLoadingIndicator()
+                    }
+                }
+            ]))
+
+            actions.append(UIAction(
+                title: NSLocalizedString("MIGRATE", comment: ""),
+                image: UIImage(systemName: "arrow.left.arrow.right")
+            ) { [weak self] _ in
+                let manga = manga.toManga()
+                let migrateView = MigrateMangaView(manga: [manga])
+                self?.present(UIHostingController(rootView: SwiftUINavigationView(rootView: AnyView(migrateView))), animated: true)
+            })
 
             if let url = manga.url {
                 actions.append(UIAction(
